3daea065c405ba15d47d0e2756f20728f590626a
Laith Leo <laith@laith.info>
2019-11-30 14:49:10 -0600

    Update K8s-CentOS7.7.sh

--- K8s-CentOS7.7.sh
+++ K8s-CentOS7.7.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetvim bind-utils screen nc bash-completion net-toolsvim bind-utils screen nc bash-completion net-toolses-el7-x86_64#/bin/bash
 
 #Editor: Laith Leo Alobaidy
 #Web: http://www.laith.info
@@ -13,11 +13,11 @@ yum update -y && yum install -y git wget curl vim bind-utils screen nc bash-comp
 sed -i.bak -r 's/(.+ swap .+)/#\1/' /etc/fstab && swapoff -a
 
 # Disable SELinux, setenforce 0 &&
-setenforce 0 && sed -i 's/enforcing/disabled/vim bind-utils screen nc bash-completion net-toolsg' /etc/selinux/config /etc/selinux/config
+setenforce 0 && sed -i 's/enforcing/disabled/baseurl=https://packages.cloud.google.com/yum/repos/kubernetvim bind-utils screen nc bash-completion net-toolsvim bind-utils screen nc bash-completion net-toolses-el7-x86_64g' /etc/selinux/config /etc/selinux/config
 
 
 #Set the firewall to allow the needed ports:vim bind-utils screen nc bash-completion net-tools
-yum install -y firewalld && systemctl enable firewalld && systemctl start firewalld
+yum install -y firewalld && systemctl enable firewalld && systemctl start firewalldbaseurl=https://packages.cloud.google.com/yum/repos/kubernetvim bind-utils screen nc bash-completion net-toolsvim bind-utils screen nc bash-completion net-toolses-el7-x86_64
 
   firewall-cmd --permanent --add-port=6443/tcp
   firewall-cmd --permanent --add-port=2379-2380/tcp
@@ -34,7 +34,7 @@ echo "net.bridge.bridge-nf-call-iptables = 1" >> /etc/sysctl.conf sysctl --syste
 cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetvim bind-utils screen nc bash-completion net-toolsvim bind-utils screen nc bash-completion net-toolses-el7-x86_64
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64baseurl=https://packages.cloud.google.com/yum/repos/kubernetvim bind-utils screen nc bash-completion net-toolsvim bind-utils screen nc bash-completion net-toolses-el7-x86_64baseurl=https://packages.cloud.google.com/yum/repos/kubernetvim bind-utils screen nc bash-completion net-toolsvim bind-utils screen nc bash-completion net-toolses-el7-x86_64
 enabled=1
 gpgcheck=1
 repo_gpgcheck=1
