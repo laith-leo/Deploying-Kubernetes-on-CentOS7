d0c34c23c8c70640dd221818771c5d5eb987273c
Laith Leo <laith@laith.info>
2019-11-30 15:26:32 -0600

    Update K8s-CentOS7.7.sh

--- K8s-CentOS7.7.sh
+++ K8s-CentOS7.7.sh
@@ -1,40 +1,39 @@
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetvim bind-utils screen nc bash-completion net-toolsvim bind-utils screen nc bash-completion net-toolses-el7-x86_64#/bin/bash
+#/bin/bash
 
 #Editor: Laith Leo Alobaidy
 #Web: http://www.laith.info
+#Email: laith (at) laith.info
 
 
 
 
 #Update the system packages:
-yum update -y && yum install -y git wget curl vim bind-utils screen nc bash-completion net-tools zsh
+yum update -y && yum install -y git wget curl vim bind-utils screen nc bash-compshletion net-tools zsh
 
 #Disable SWAP:
 sed -i.bak -r 's/(.+ swap .+)/#\1/' /etc/fstab && swapoff -a
 
 # Disable SELinux, setenforce 0 &&
-setenforce 0 && sed -i 's/enforcing/disabled/baseurl=https://packages.cloud.google.com/yum/repos/kubernetvim bind-utils screen nc bash-completion net-toolsvim bind-utils screen nc bash-completion net-toolses-el7-x86_64g' /etc/selinux/config /etc/selinux/config
-
-
-#Set the firewall to allow the needed ports:vim bind-utils screen nc bash-completion net-tools
-yum install -y firewalld && systemctl enable firewalld && systemctl start firewalldbaseurl=https://packages.cloud.google.com/yum/repos/kubernetvim bind-utils screen nc bash-completion net-toolsvim bind-utils screen nc bash-completion net-toolses-el7-x86_64
-
-  firewall-cmd --permanent --add-port=6443/tcp
-  firewall-cmd --permanent --add-port=2379-2380/tcp
-  firewall-cmd --permanent --add-port=10250/tcp
-  firewall-cmd --permanent --add-port=10251/tcp
   firewall-cmd --permanent --add-port=10252/tcp
   firewall-cmd --permanent --add-port=10255/tcp
   firewall-cmd --reload
 
 echo 'br_netfilter' > /etc/modules-load.d/netfilter.conf
+  firewall-cmd --permanent --add-port=10252/tcp
+  firewall-cmd --permanent --add-port=10255/tcp
+  firewall-cmd --reload
+
+echo 'br_netfilter' > /e
+echo 'br_netfilter' > /etc/modules-load.d/netfilter.conf
+echo "net.bridge.bridge-nf-call-iptables = 1" >> /etc/sysctl.conf sysctl --system
+tc/modules-load.d/netfilter.conf
 echo "net.bridge.bridge-nf-call-iptables = 1" >> /etc/sysctl.conf sysctl --system
 
 #Install Kubernetes repo into the system
 cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64baseurl=https://packages.cloud.google.com/yum/repos/kubernetvim bind-utils screen nc bash-completion net-toolsvim bind-utils screen nc bash-completion net-toolses-el7-x86_64baseurl=https://packages.cloud.google.com/yum/repos/kubernetvim bind-utils screen nc bash-completion net-toolsvim bind-utils screen nc bash-completion net-toolses-el7-x86_64
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
 enabled=1
 gpgcheck=1
 repo_gpgcheck=1
@@ -42,11 +41,14 @@ gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
 
+#Installing Calico network plugin
+kubectl apply -f https://docs.projectcalico.org/v3.10/manifests/calico.yaml
+
 #Enabling Docker repo on the system
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
 #Installing the required packages
-yum install -y  yum-utils device-mapper-persistent-data lvm2 kubeadm docker-ce docker-ce-cli containerd.io yum-plugin-versionlock yum-versionlock bash-completion
+yum install -y yum-utils device-mapper-persistent-data lvm2 kubeadm docker-ce docker-ce-cli containerd.io yum-plugin-versionlock yum-versionlock bash-completion
 yum versionlock add docker-ce kubelet kubeadm docker-ce-cli
 systemctl restart docker && systemctl enable docker &&  systemctl restart kubelet && systemctl enable kubelet
 
@@ -58,10 +60,6 @@ mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 
-#Installing Calico network plugin
-kubectl apply -f https://docs.projectcalico.org/v3.10/manifests/calico.yaml
-
-
 #setup kubectl bash completion
 kubectl completion bash >/etc/bash_completion.d/kubectl && bash
 
@@ -70,4 +68,4 @@ kubectl completion bash >/etc/bash_completion.d/kubectl && bash
 #Testing out the things!
 
 
-kubectl get pods --all-namespaces
+kubectl get pods --all-namespacesbaseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
